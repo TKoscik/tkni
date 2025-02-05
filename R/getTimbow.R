64b71e19616b66ef40ce663ea2f0e04f24ac40c3
tkoscik <tkoscik@uams.edu>
2025-02-05 11:27:09 -0600

    commit this

--- R/getTimbow.R
+++ R/getTimbow.R
@@ -14,7 +14,7 @@ for (i in 1:length(args)) {
   if (args[i] %in% c("sat", "saturation", "colorfulness")) { saturation <- as.numeric(args[i+1]) }
   if (args[i] %in% c("lo", "lum.lo", "luminosity.lo")) { luminosity.lo <- as.numeric(args[i+1]) }
   if (args[i] %in% c("hi", "lum.hi", "luminosity.hi")) { luminosity.hi <- as.numeric(args[i+1]) }
-  if (args[i] %in% c("arc", "cycles", "n.cycles")) { cycles <- eval(parse(text=args[i+1])) }
+  if (args[i] %in% c("arc", "cyc", "cycles", "n.cycles")) { cycles <- eval(parse(text=args[i+1])) }
   if (args[i] %in% c("dir", "direction" )) { direction <- args[i+1] }
 }
 
